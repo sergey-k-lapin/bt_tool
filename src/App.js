fe51a33d6de2c8ae5a9eed6ac31a6368a67fc429
Sergey <sergey.k.lapin@gmail.com>
2024-09-29 17:33:12 +0300

    Splitter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useStat, createRef } from 'react';
 import { graphStore } from './data/graphStore';
 
 import TreeSVG from './components/TreeSVG';
@@ -8,6 +8,7 @@ import Toolbar from './components/Toolbar';
 import NodesPalette from './components/NodesPalette';
 import DiagramSettings from './components/DiagramSettings';
 import CodeEditor from './components/Editor';
+
 
 function App() {
   let nodesData = JSON.parse(localStorage.getItem('_nodesData'));
@@ -21,15 +22,74 @@ function App() {
   graphStore.setNodes(nodesData);
   graphStore.setEdges(edgesData);
 
+  const panels = useRef({
+    left: {
+      element: createRef(),
+      size: 50,
+    },
+    right: {
+      element: createRef(),
+      size: 50,
+    }
+  });
+
+  const m = useRef({
+    x:0,
+    y:0,
+  });
+
+  const percentsPerPixel = useRef(0);
+
+  function size2css(size) {
+    return `calc(${size}% - 5px)`;
+  }
+  
+  function changePanelSize(size, panel) {
+    panel.element.current.style.width = size2css(size);
+  }
+  
+  function changePanels(delta){
+    panels.current.left.size -= delta*percentsPerPixel.current;
+    panels.current.right.size += delta*percentsPerPixel.current;
+    changePanelSize(panels.current.left.size, panels.current.left);
+    changePanelSize(panels.current.right.size, panels.current.right);
+  }
+
+
   console.log('Перерисовал шайнан.');
+
+  const onMouseMove = (e) => {
+      changePanels(m.current.x - e.clientX);
+      m.current.x = e.clientX;
+      m.current.y = e.clientY;
+  }
+
 
   return (
     <>
-      <TreeSVG />
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ height: '100vh', width: size2css(panels.current.left.size) }} ref={panels.current.left.element}>
+          <TreeSVG />
+        </div>
+        <div style={{ height: '100vh', width: '10px', cursor: 'col-resize' }}
+          onMouseDown={(e) => {
+            percentsPerPixel.current = 100.0 / e.target.parentNode.clientWidth;
+            m.current.x = e.clientX;
+            m.current.y = e.clientY;
+            window.addEventListener('mousemove', onMouseMove);
+            window.addEventListener('mouseup', () =>{
+              window.removeEventListener('mousemove', onMouseMove);
+            }, {once: true});
+          }}
+        ></div>
+        <div style={{ height: '100vh', width: size2css(panels.current.right.size) }} ref={panels.current.right.element}>
+          <CodeEditor />
+        </div>
+      </div>
+
       <Toolbar />
       <DiagramSettings />
       <NodesPalette />
-      <CodeEditor />
     </>
   );
 }
